ed50ca4a8b7a3162c55cacbc385352b2afb32265
Ender Ahmet Yurt <enderyurt@gmail.com>
2017-09-16 11:51:14 +0300

    Add test for inheritance class.

---
+++ inheritance/f1_car_test.rb
@@ -0,0 +1,16 @@
+require_relative '../basic/car.rb'
+require_relative 'f1_car.rb'
+
+car1 = Car.new("Mercedes", "C2000", "2017")
+car1.distance = 0
+car1.speed = 0
+puts car1.get_info
+
+f1car1 = F1Car.new("Mercedes", "C2000", "2017")
+f1car1.distance = 20000
+f1car1.speed = 310
+f1car1.pilot = "Mehmet"
+puts f1car1.get_info
+
+car2 = f1car1
+puts car2.get_info
