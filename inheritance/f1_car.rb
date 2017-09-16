cd0be67f9b3c00a7755ef5ca231e778b564bd428
Ender Ahmet Yurt <enderyurt@gmail.com>
2017-09-16 11:51:02 +0300

    Add child class for Car.

---
+++ inheritance/f1_car.rb
@@ -0,0 +1,15 @@
+require_relative '../basic/car.rb'
+
+class F1Car < Car
+  attr_accessor :pilot
+
+  def accelerate(new_speed)
+    puts "Faster acceleration!"
+    @speed = new_speed
+  end
+
+  def get_info
+    info = super
+    "#{info} Pilot is #{pilot}"
+  end
+end
