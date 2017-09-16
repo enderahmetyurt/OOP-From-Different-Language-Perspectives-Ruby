70283c6ac75447e02d8be311430372cfcffe565c
Ender Ahmet Yurt <enderyurt@gmail.com>
2017-09-16 21:07:43 +0300

    Implement Vehicle module.

---
+++ interfaces/vehicle.rb
@@ -0,0 +1,17 @@
+module Vehicle
+  def go(new_instance)
+    raise "Not implemented"
+  end
+
+  def accelerate(new_speed)
+    raise "Not implemented"
+  end
+
+  def stop
+    raise "Not implemented"
+  end
+
+  def get_info
+    raise "Not implemented"
+  end
+end
